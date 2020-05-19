f0999ed1f85d6044d47df280ff7a95645fa38d9f
euskate <euskate07@gmail.com>
2020-05-19 10:58:41 +0900

    initial commit

---
+++ src/config/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for mydjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+# 상위폴더이름 config
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
